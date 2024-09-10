ac612cd66df92d98b7accff8d265997684068ab0
조우식 <chousik01@gmail.com>
2024-09-10 15:51:46 +0900

    new | object

--- numpy.py
+++ numpy.py
@@ -1,23 +1,52 @@
 import basicData
+import tutorial
+# count = 0
+
+# def process_dict(d):
+#     global count
+#     for key, value in d.items():
+#         if "교통" in key:
+#             if isinstance(value, dict):
+#                 # 값이 딕셔너리인 경우, 재귀적으로 처리
+#                 process_dict(value)
+#             else:
+#                 # 값이 숫자일 경우, count에 추가
+#                 if isinstance(value, (int, float)):
+#                     count += value
+
+# # 모든 딕셔너리 처리
+# for item in basicData.basic_data:
+#     if isinstance(item, dict):
+#         process_dict(item)
+
+
+# print("최종 count 값:", count)
+
+
 
 count = 0
+container = []
 
-def process_dict(d):
+def recursionDef(data):
+    
+    global container
     global count
-    for key, value in d.items():
-        if "교통" in key:
-            if isinstance(value, dict):
-                # 값이 딕셔너리인 경우, 재귀적으로 처리
-                process_dict(value)
-            else:
-                # 값이 숫자일 경우, count에 추가
-                if isinstance(value, (int, float)):
+    
+    for element in data:
+        if type(element) == list:
+            recursionDef(element)
+        elif type(element) == dict :
+            for key, value in element.items():
+                if "교통" in key and type(value) == int or type(value)==float:
+                    container.append(key)
                     count += value
+                if type(value) == dict or type(value) == list:
+                    recursionDef(value)
 
-# 모든 딕셔너리 처리
-for item in basicData.basic_data:
-    if isinstance(item, dict):
-        process_dict(item)
+                            
 
+recursionDef(tutorial.tutorial)
 
-print("최종 count 값:", count)
+print(container)
+print(count)
+print(len(container))
