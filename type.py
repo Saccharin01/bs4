3fe94b8d1009846db2940c6f61afc6a2b6578e02
조우식 <chousik01@gmail.com>
2024-09-10 10:39:49 +0900

    modify

--- type.py
+++ type.py
@@ -1,21 +1,22 @@
+
+
+
 user_input = input("입력 해주세요 : ")
+
 
 
 try :
   test = int(user_input)
-  if type(test) == float : 
-    print("입력하신 내용은 숫자입니다.")
-
+  print("입력하신 내용은 숫자형 입니다.")
+    
 except ValueError:
+  try:
     test = float(user_input)
-
-
-    if type(test) == float :
-      print("입력하신 내용은 소수점이 있는 숫자입니다.")
-      
-    elif ValueError:
-      test = str(user_input)
-      print("입력하신 내용은 문자열 입니다.")
+    print("입력하신 내용은 소수점이 있는 숫자입니다.")
+  
+  except ValueError:
+    test = str(user_input)
+    print("입력하신 내용은 문자열 입니다.")
 
 
 
